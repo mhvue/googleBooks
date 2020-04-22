a08add762303aa792541be09cdfcc073ea4ff7da
Mai Houa Vue <mhvue4@gmail.com>
2020-04-21 23:44:40 -0500

    used postman to test api routes with monogodb methods. GETs, POSTs, DELETE working

--- routes/api.js
+++ routes/api.js
@@ -5,25 +5,25 @@ module.exports = function (app){
 
 //get ALL the books saved to db
 app.get("/api/allbooks", (req, res) => {
-    console.log("getting")
-    // db.Books.findAll({})
-    // .then(db => res.json(db))
-    // .catch(error =>console.log(error))
+    // console.log("getting")
+    db.Books.find({})
+    .then(db => res.json(db))
+    .catch(error =>console.log(error))
 });
 
 //get ONE book  saved to db 
 app.get("/api/book/:id", (req, res) =>{
-    console.log("getting one only")
-    // const bookOne = req.params._id
+    // console.log("getting one only")
+    const bookOne = req.params._id
 
-    // db.Books.findOne({"id":bookOne })
-    // .then(db =>res.json(db))
-    // .catch(error => console.log(error))
+    db.Books.findOne({"id":bookOne })
+    .then(db =>res.json(db))
+    .catch(error => console.log(error))
 });
 
 //saving ONE book to the db 
 app.post("/api/savebook", (req, res)=>{
-    console.log("postingggg")
+    // console.log("postingggg")
 
     db.Books.create(req.body)
     .then(db => res.json(db))
@@ -31,9 +31,9 @@ app.post("/api/savebook", (req, res)=>{
 });
 
 //delete one book from the db 
-app.delete("/api/book:id", (req, res) => {
-    console.log("deletinggggggg")
-    // const bookDelete = req.params._id
-    // db.Books.deleteOne({"id": bookDelete})
+app.delete("/api/deletebook/:id", (req, res) => {
+    // console.log("deletinggggggg")
+    const bookDelete = req.params._id
+    db.Books.deleteOne({"id": bookDelete})
 });
 }
