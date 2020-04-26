f6be6388505b5e62f96188d55e9d2e64fd022dc6
Mai Houa Vue <mhvue4@gmail.com>
2020-04-25 23:22:40 -0500

    attempt to deploy to heroku

---
+++ client/build/precache-manifest.bbb137e0a7359a2d27cb57c9495be7c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bd399e49f16e3d5feeef82bb3dcad9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f2c7de7b01e141407f",
+    "url": "/static/css/main.09bbfe08.chunk.css"
+  },
+  {
+    "revision": "5c5dceacf52e4e1b7aeb",
+    "url": "/static/js/2.9d657b31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d657b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f2c7de7b01e141407f",
+    "url": "/static/js/main.98e1fabc.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "999454f559c0b1f0b12056ec5e35e97f",
+    "url": "/static/media/stackedBooks.999454f5.jpg"
+  }
+]);
