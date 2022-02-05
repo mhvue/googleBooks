da6c267c0c023d8f98e4f0e52c7cff8a6229fc14
mhvue <mhvue4@gmail.com>
2022-02-05 14:02:26 -0600

    adjusted background image and jumbotron.working on navbar collapse issue.

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,10 +5,13 @@ import "./componentStyle.css"
 function Nav (){
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light">
                 <Link className="navbar-brand" to="/"><h2>Google Books</h2></Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <Link className="nav-link text-light" to="/">Search for Books<span className="sr-only">(current)</span></Link>
                     </li>
